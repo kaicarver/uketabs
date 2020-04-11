a437df4dd9320bf66fcaf02722e708ff4cc80185
Kai Carver <kaicarver@gmail.com>
2020-04-11 12:29:28 +0200

    React in multiple places

--- like_button.js
+++ like_button.js
@@ -21,5 +21,16 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+//const domContainer = document.querySelector('#like_button_container');
+//ReactDOM.render(e(LikeButton), domContainer);
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
