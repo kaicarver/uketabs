34e3b3b4da388c4afe7608b998f0ee82ad7ad07f
Kai Carver <kaicarver@gmail.com>
2020-04-12 23:34:50 +0200

    even more templaty; think of using rect-helmet-async

--- src/App.js
+++ src/App.js
@@ -7,6 +7,48 @@ function App() {
   let author = "Kool and the Gang";
   let date = "1980";
   const tabsImage = "celebration.png";
+  const score = `
+  [Intro]
+  
+  | N.C. | % | % | % |
+  (drums + brass lick)
+  | N.C. | % | % | % |
+  (drums + brass lick)
+  |  C  Fadd9 C | Fadd9 C G   |
+  (riff*)
+  |  C  Fadd9 C | Fadd9 C G   |
+  (riff)
+
+  [Chorus]
+  C Fadd9 C    Fadd9 C    G
+    Ce  - le - brate good times, come on
+  C Fadd9   C Fadd9    C   G
+                It's a celebration
+  C Fadd9 C    Fadd9 C    G
+    Ce  - le - brate good times, come on
+  C Fadd9   C Fadd9    C   G
+                Let's celebrate
+
+  [Verse 1]
+            Fadd9                C       G
+  There's a party goin' on right here
+        Fadd9                          C       G
+  A celebration to last throughout the years
+                Fadd9               C        G
+  So bring your good times and your laughter too
+  Fadd9                         C          G
+     We're gonna celebrate your party with you, come on now
+
+  [Bridge]
+  Fadd9   C    G
+      Celebration
+  Fadd9                      C           G
+     Let's all celebrate and have a good time
+  Fadd9   C    G
+      Celebration
+  Fadd9                        C           G
+     We're gonna celebrate and have a good time
+`;
   return (
     <div>
       <Helmet>
@@ -23,48 +65,7 @@ function App() {
           <img src={tabsImage} alt="tabs" />
         </p>
       </div>
-<pre>{`
-     [Intro]
-     
-     | N.C. | % | % | % |
-     (drums + brass lick)
-     | N.C. | % | % | % |
-     (drums + brass lick)
-     |  C  Fadd9 C | Fadd9 C G   |
-     (riff*)
-     |  C  Fadd9 C | Fadd9 C G   |
-     (riff)
- 
-     [Chorus]
-     C Fadd9 C    Fadd9 C    G
-       Ce  - le - brate good times, come on
-     C Fadd9   C Fadd9    C   G
-                   It's a celebration
-     C Fadd9 C    Fadd9 C    G
-       Ce  - le - brate good times, come on
-     C Fadd9   C Fadd9    C   G
-                   Let's celebrate
- 
-     [Verse 1]
-               Fadd9                C       G
-     There's a party goin' on right here
-           Fadd9                          C       G
-     A celebration to last throughout the years
-                   Fadd9               C        G
-     So bring your good times and your laughter too
-     Fadd9                         C          G
-        We're gonna celebrate your party with you, come on now
- 
-     [Bridge]
-     Fadd9   C    G
-         Celebration
-     Fadd9                      C           G
-        Let's all celebrate and have a good time
-     Fadd9   C    G
-         Celebration
-     Fadd9                        C           G
-        We're gonna celebrate and have a good time
-`}</pre>
+<pre>{score}</pre>
     </div>
   );
 }
