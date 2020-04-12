c4b9114163eda22fbc41165ca285cfe4bf8c3601
Kai Carver <kaicarver@gmail.com>
2020-04-12 22:39:15 +0200

    set page title using react-helmet from the NFL...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Kai's Uke Tabs</title>
+      </Helmet>
       <p>Web App goes here</p>
     </div>
   );
