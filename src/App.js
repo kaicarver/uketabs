24d9662ed210cf4a94c9ce85c6b8ee15064fb5af
Kai Carver <kaicarver@gmail.com>
2020-04-15 09:55:42 +0200

    experiment with a form

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class Song extends React.Component {
         <h1>{this.state.s.song}</h1>
         <h2>by {this.state.s.author}</h2>
         <div className="date">{this.state.s.date}</div>
+        <NameForm />,
         <p>
           <a href={"https://" + this.state.s.url}>{this.state.s.url}</a><br />
         </p>
@@ -39,6 +40,37 @@ class Song extends React.Component {
       </div>
       <pre style={{ fontSize: this.state.s.scoreFontSize || '100%' }}>{this.state.s.score}</pre>
     </div>
+  }
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
 
