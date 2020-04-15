94f51d958eba0c4e8372bfe8f405b7f115de79b6
Kai Carver <kaicarver@gmail.com>
2020-04-15 10:28:34 +0200

    click on title; another form field

--- src/App.js
+++ src/App.js
@@ -18,19 +18,21 @@ class Song extends React.Component {
     this.state = {songNumber: this.props.songNumber, s: scores[this.props.songNumber]};
   }
   render() {
-    return <div
-      onClick={() => {
-        //alert("hi from song " + this.state.songNumber);
-        let num = (this.state.songNumber + 1) % scores.length;
-        this.setState({songNumber: num, s: scores[num]}) }}>
+    return <div>
       <Helmet>
         <title>{this.state.s.song} by {this.state.s.author}</title>
       </Helmet>
       <div id="title">
-        <h1>{this.state.s.song}</h1>
+        <h1 onClick={() => {
+          //alert("hi from song " + this.state.songNumber);
+          let num = (this.state.songNumber + 1) % scores.length;
+          this.setState({ songNumber: num, s: scores[num] })
+        }}>
+          {this.state.s.song}</h1>
         <h2>by {this.state.s.author}</h2>
         <div className="date">{this.state.s.date}</div>
         <NameForm />,
+        <EssayForm />,
         <p>
           <a href={"https://" + this.state.s.url}>{this.state.s.url}</a><br />
         </p>
@@ -67,6 +69,39 @@ class NameForm extends React.Component {
         <label>
           Name:
           <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
