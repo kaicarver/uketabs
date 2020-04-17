8272036c8e9d84ddcd346a867035e7f851a6f4d8
Kai Carver <kaicarver@gmail.com>
2020-04-17 07:55:42 +0200

    working way to lifting state up

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ class Song extends React.Component {
         <FlavorForm />
         <Reservation />
         <FileInput />
+        <Calculator />
         <p>
           <Link url={"https://" + this.state.s.url} label={this.state.s.url} />
           <br />
@@ -219,6 +220,39 @@ class FileInput extends React.Component {
       </form>
     );
   }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {temperature: ''};
+  }
+
+  handleChange(e) {
+    this.setState({temperature: e.target.value});
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={temperature}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
 }
 
 export default App;
