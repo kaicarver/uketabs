2299239581fda8dbcb45c009752d99b8d42958a1
Kai Carver <kaicarver@gmail.com>
2020-04-17 10:01:52 +0200

    start world clocks demo

--- src/Demos.js
+++ src/Demos.js
@@ -10,6 +10,7 @@ function Demos() {
             <Reservation />
             <FileInput />
             <Calculator />
+            <WorldClocks />
         </div>
     );
 }
@@ -251,5 +252,46 @@ function BoilingVerdict(props) {
   }
   return <p>The water would not boil.</p>;
 }
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <FormattedDate date={this.state.date} />
+        </div>
+      );
+    }
+  }
+  function WorldClocks() {
+    return (
+      <div>
+        <Clock />
+        <Clock />
+        <Clock />
+      </div>
+    );
+  }
 
 export default Demos;
