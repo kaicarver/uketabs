7c79db04fe357eec51e6bbde243d5ec60998ac61
Kai Carver <kaicarver@gmail.com>
2020-04-18 08:44:33 +0200

    About page!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './index.css';
 import App from './App';
@@ -7,12 +8,24 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <BrowserRouter>
+      <HelmetProvider>
+        <Switch>
+        <Route path="/" component={App} exact />
+        <Route path="/about" component={About} exact />
+
+        </Switch>
+      </HelmetProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function About() {
+  return (
+    <div>About!</div>
+  )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
